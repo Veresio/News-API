8113f7c00882c8739b0788ad19848365f42b1ffa
Laurentiu Patrascu <lpatrascu55@yahoo.com>
2023-10-04 12:20:06 +0100

    added typeof tests to the properties

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -124,13 +124,21 @@ describe("GET /api/articles", () => {
         expect(body.articles.length).not.toBe(0);
         body.articles.forEach((article) => {
           expect(article).toHaveProperty("author");
+          expect(typeof article.author).toBe("string");
           expect(article).toHaveProperty("title");
+          expect(typeof article.title).toBe("string");
           expect(article).toHaveProperty("article_id");
+          expect(typeof article.article_id).toBe("number");
           expect(article).toHaveProperty("topic");
+          expect(typeof article.topic).toBe("string");
           expect(article).toHaveProperty("created_at");
+          expect(typeof article.created_at).toBe("string");
           expect(article).toHaveProperty("votes");
+          expect(typeof article.votes).toBe("number");
           expect(article).toHaveProperty("article_img_url");
+          expect(typeof article.article_img_url).toBe("string");
           expect(article).toHaveProperty("comment_count");
+          expect(typeof article.comment_count).toBe("string");
           expect(article).not.toHaveProperty("body");
         });
       });
