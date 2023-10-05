140cbd02acc48b8bd6cc50b184858f582b4fd650
Laurentiu Patrascu <lpatrascu55@yahoo.com>
2023-10-05 16:34:01 +0100

    removed extra console.log

--- server/models.js
+++ server/models.js
@@ -17,7 +17,6 @@ exports.fetchTopics = () => {
 };
 
 exports.fetchArticles = (topic) => {
-  console.log(topic);
   let query =
     "SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id ";
 
